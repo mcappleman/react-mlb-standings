2bc70d15b0bf2fac7485992d5fac58521ddbada6
Matt Cappleman <matt@fanhero.com>
2017-06-20 16:31:32 -0400

    Figuring out how to use enzyme with async unit tests

--- src/components/DailyGames/DailyGames.test.js
+++ src/components/DailyGames/DailyGames.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment';
+import { mount } from 'enzyme';
 import DailyGames from './DailyGames';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<DailyGames />, div);
 });
+
+describe('Get the games for a particular day', () => {
+
+	it('should get the games for tomorrow', (done) => {
+
+		var d = moment().add(1, 'days');
+
+		// var dailyGames = new DailyGames();
+		const wrapper = mount(<DailyGames />);
+
+		// wrapper.getGames
+
+	});
+
+})
